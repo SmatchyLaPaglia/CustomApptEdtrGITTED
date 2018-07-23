27ee3087c1d0319f4a5ab56d1224f08ddb92d778
Jesse on MacBook <jwc@jessewonderclark.com>
2018-07-23 06:59:25 -0400

    editor placeholder works

---
+++ AdditionalAttribute/TeenyPlanner/EditorPlaceholder.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace TeenyPlanner
+{
+    public class EditorPlaceHolder : Editor
+    {
+
+        String placeHolderText = "";
+
+        public EditorPlaceHolder(String placeholder)
+        {
+            Text = placeholder;
+            TextColor = Color.LightGray;
+            this.Focused += EditorPlaceHolder_Focused;
+            this.Unfocused += EditorPlaceHolder_Unfocused;
+            this.placeHolderText = placeholder;
+        }
+
+        private void EditorPlaceHolder_Focused(object sender, FocusEventArgs e) //triggered when the user taps on the Editor to interact with it
+        {
+            if (Empty())
+            {
+                base.Text = "";
+                this.TextColor = Color.Black;
+            }
+        }
+
+        private void EditorPlaceHolder_Unfocused(object sender, FocusEventArgs e) //triggered when the user taps "Done" or outside of the Editor to finish the editing
+        {
+            if (Empty()) //if there is text there, leave it, if the user erased everything, put the placeholder Text back and set the TextColor to gray
+            {
+                this.Text = placeHolderText;
+                this.TextColor = Color.LightGray;
+            }
+        }
+
+        public String PlaceHolderText
+        {
+            get
+            {
+                return this.placeHolderText;
+            }
+            set
+            {
+                if (this.Empty())
+                {
+                    this.Text = value;
+                    this.TextColor = Color.LightGray;
+                }
+                this.placeHolderText = value;
+
+            }
+        }
+
+        public bool Empty()
+        {
+            return (this.Text.Equals("") || this.Text.Equals(this.placeHolderText));
+        }
+
+        public virtual new string Text
+        {
+            get
+            {
+                return base.Text;
+            }
+            set
+            {
+                base.Text = value;
+                if (Empty())
+                {
+                    this.TextColor = Color.LightGray;
+                    base.Text = this.placeHolderText;
+                }
+                else
+                {
+                    this.TextColor = Color.Black;
+                }
+
+            }
+
+        }
+    }
+}
+
--- AdditionalAttribute/TeenyPlanner/TeenySchedule.xaml.cs
+++ AdditionalAttribute/TeenyPlanner/TeenySchedule.xaml.cs
@@ -1,19 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using Xamarin.Forms;
+using TeenyPlanner;
 
 namespace TeenyPlanner
 {
     public partial class TeenySchedule : ContentView
     {
+        string editorPlaceholderText = "enter address";
+        Color editorPlaceholderTextColor = Color.Gray;
+
+        //Start app and setup placholder editor text and style
         public TeenySchedule()
         {
             InitializeComponent();
+            setupPlaceholderEditor();
         }
 
+        //Remove editor placeholder text when focused
 		void Handle_Focused(object sender, Xamarin.Forms.FocusEventArgs e)
 		{
-			addressEditor.Text = "";
+            if (addressEditor.Text == editorPlaceholderText) {
+                addressEditor.Text = "";
+                addressEditor.TextColor = Color.Black;
+            }
 		}
+
+        void Handle_Unfocused(object sender, Xamarin.Forms.FocusEventArgs e)
+        {
+            if (addressEditor.Text == "")
+            {
+                setupPlaceholderEditor();
+                return;
+            }        }
+
+        //Change editor text to placeholder if editor is empty
+        //void Handle_Unfocused(object sender, Xamarin.Forms.TextChangedEventArgs e)
+        //{
+            //if (addressEditor.Text == "")
+            //{
+            //    setupPlaceholderEditor();
+            //    return;
+            //}
+        //}
+
+        //DRY for setting placeholder editor text and color
+        void setupPlaceholderEditor() {
+            addressEditor.Text = editorPlaceholderText;
+            addressEditor.TextColor = editorPlaceholderTextColor;
+        }
     }
 }
